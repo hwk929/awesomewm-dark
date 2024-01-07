6e250074a5dfee71ea245791e05e43b341c71bc6
Cyan903 <cyanp0515@gmail.com>
2024-01-07 06:36:08 -0500

    rework: Split bindings, rules, signals, and wibar from rc.lua

--- lib/menu_drop.lua
+++ lib/menu_drop.lua
@@ -1,8 +1,9 @@
-local globs = require("lib.global")
 local awful = require("awful")
 local beautiful = require("beautiful")
 local menubar = require("menubar")
 local hotkeys_popup = require("awful.hotkeys_popup")
+
+local globs = require("lib.global")
 
 -- {{{ Menu
 -- Create a launcher widget and a main menu
