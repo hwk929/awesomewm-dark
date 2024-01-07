07e027ede48aa31d1bd9c6b920d3fad6a767d92a
Cyan903 <cyanp0515@gmail.com>
2024-01-07 12:07:13 -0500

    feat: Basic wibar improvements

--- config/awesome.lua
+++ config/awesome.lua
@@ -1,27 +1,17 @@
--- Standard awesome library
 local gears = require("gears")
 local awful = require("awful")
-
--- Theme handling library
 local beautiful = require("beautiful")
 
--- {{{ Variable definitions
--- Themes define colours, icons, font and wallpapers.
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+beautiful.useless_gap = 5
 
--- This is used later as the default terminal and editor to run.
+-- Defaults
+local modkey = "Mod4"
 local terminal = "xterm"
 local editor = os.getenv("EDITOR") or "nano"
 local editor_cmd = terminal .. " -e " .. editor
 
--- Default modkey.
--- Usually, Mod4 is the key with a logo between Control and Alt.
--- If you do not like this or do not have such a key,
--- I suggest you to remap Mod4 to another key using xmodmap or other tools.
--- However, you can use another modifier like Mod1, but it may interact with others.
-local modkey = "Mod4"
-
--- Table of layouts to cover with awful.layout.inc, order matters.
+-- Layout
 awful.layout.layouts = {
     awful.layout.suit.floating,
     awful.layout.suit.tile,
@@ -40,7 +30,6 @@ awful.layout.layouts = {
     -- awful.layout.suit.corner.sw,
     -- awful.layout.suit.corner.se,
 }
--- }}}
 
 return {
     terminal = terminal,
