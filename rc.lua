c8e2b9ceebafd4f642c61a6c5304bb3ef0405693
Cyan903 <cyanp0515@gmail.com>
2024-01-07 07:00:16 -0500

    chore: Add themes

--- rc.lua
+++ rc.lua
@@ -2,21 +2,6 @@
 -- found (e.g. lgi). If LuaRocks is not installed, do nothing.
 pcall(require, "luarocks.loader")
 
--- Standard awesome library
--- local gears = require("gears")
--- local awful = require("awful")
--- require("awful.autofocus")
--- Widget and layout library
--- local wibox = require("wibox")
--- Theme handling library
--- local beautiful = require("beautiful")
-
--- local menubar = require("menubar")
--- local hotkeys_popup = require("awful.hotkeys_popup")
--- Enable hotkeys help widget for VIM and other apps
--- when client with a matching name is opened:
--- require("awful.hotkeys_popup.keys")
-
 -- Errors
 require("lib.error")
 
