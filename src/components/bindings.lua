04a4e626f19c580e0e6665312f2f7c0955a02b1f
Cyan903 <cyanp0515@gmail.com>
2024-01-08 12:43:00 -0500

    fix: Fix typo, add show_hotkeys

--- src/components/bindings.lua
+++ src/components/bindings.lua
@@ -1,10 +1,13 @@
 local gears = require("gears")
 
+local cfg = require("src.util.config")
 local clientkeys = require("src.components.bindings.client.keys")
-local clientbuttons = require("src.components.bindings.client.keys")
+local clientbuttons = require("src.components.bindings.client.buttons")
 local mousekeys = require("src.components.bindings.client.mouse")
 
-require("awful.hotkeys_popup.keys")
+if cfg.config.window.show_hotkeys then
+    require("awful.hotkeys_popup.keys")
+end
 
 local globalkeys = gears.table.join(
     require("src.components.bindings.global.awesome"),
