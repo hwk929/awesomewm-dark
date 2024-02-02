8761e61d30f89c0269087ef059671535a2ee04aa
Cyan903 <cyanp0515@gmail.com>
2024-02-02 03:38:37 -0500

    rework: Remove context menu keyboard shortcut

--- src/components/bindings/global/awesome.lua
+++ src/components/bindings/global/awesome.lua
@@ -3,20 +3,11 @@ local awful = require("awful")
 local hotkeys_popup = require("awful.hotkeys_popup")
 
 local cfg = require("src.util.config")
-local drop = require("src.components.drop")
 
 return gears.table.join(
     awful.key({ cfg.config.modkey }, "s", hotkeys_popup.show_help, { description = "Show help", group = "Awesome" }),
-    awful.key({ cfg.config.modkey, "Control" }, "r", awesome.restart, { description = "Reload awesome", group = "Awesome" }),
-    awful.key({ cfg.config.modkey, "Shift" }, "q", awesome.quit, { description = "Quit awesome", group = "Awesome" }),
-    awful.key({ cfg.config.modkey }, "w",
-        function()
-            drop.mymainmenu:show()
-        end,
-
-        { description = "Show main menu", group = "Awesome" }
-    ),
-
+    awful.key({ cfg.config.modkey, "Control" }, "r", awesome.restart, { description = "Reload", group = "Awesome" }),
+    awful.key({ cfg.config.modkey, "Shift" }, "q", awesome.quit, { description = "Quit", group = "Awesome" }),
     awful.key({ cfg.config.modkey }, "x",
         function()
             awful.prompt.run {
